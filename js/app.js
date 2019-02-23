231984f766871bc7e65d244b1566211d1d5d0562
Natarajan <nsabari@amazon.com>
2019-02-22 16:18:02 -0800

    Lab 4 updates with functions

--- js/app.js
+++ js/app.js
@@ -5,127 +5,106 @@ console.log('user name:', userName);
 alert('Hi ' + userName + ' I\'m going to ask you few questions');
 var noOfCorrectAns = 0; 
 
-var isAdult = prompt('Do you think i am over 18 years old? Please enter yes/no');
-console.log('18+ years?:', isAdult);
-if (isAdult.toLowerCase() === 'yes' || isAdult.toLowerCase() === 'y') {
-  alert('Yes. I am an Adult.');
-  console.log('I am an Adult.');
-  noOfCorrectAns += 1;
-} else if (isAdult.toLowerCase() === 'no' || isAdult.toLowerCase() === 'n') {
-  alert('Wrong. I am a Not Minor.');
-  console.log('Wrong. I am a Not Minor..');
-} else {
-  alert('You input is invalid');
-  console.log('Invalid input:', isAdult);
-}
+function promptYesOrNoQuestionAboutMe(question, rightAnswer, otherValidResponses) {
 
-var isUSCitizen = prompt('Am i a US Citizen? Please enter yes/no');
-console.log('US Citizen?:', isUSCitizen);
-if (isUSCitizen.toLowerCase() === 'yes' || isUSCitizen.toLowerCase() === 'y') {
-  alert('Wrong! I am not US Citizen.');
-  console.log('Wrong! I am not US Citizen.');
-} else if (isUSCitizen.toLowerCase() === 'no' || isUSCitizen.toLowerCase() === 'n') {
-  alert('Correct, I am  not US Citizen.');
-  console.log('Correct, I am not US Citizen.');
-  noOfCorrectAns += 1;
-} else {
-  alert('You input is invalid');
-  console.log('Invalid input:', isUSCitizen);
-}
+var yesOrNoText =  " Please enter yes/no ";
+console.log('question:', question);
+var getAnswer = prompt( question + yesOrNoText );
+console.log('Answer for question:', question, " is ", getAnswer);
+var returnMessage, rightAnswerFlag, validResponseFlag;
 
-var isGarduate  = prompt('Do i have a Graduate degree? Please enter yes/no');
-console.log('Graduate?:', isGarduate );
-if (isGarduate.toLowerCase() === 'yes' || isGarduate.toLowerCase() === 'y') {
-  alert('Correct, I am a Graduate.');
-  console.log('Correct, I am a Graduate.');
-  noOfCorrectAns += 1;
-} else if (isGarduate.toLowerCase() === 'no' || isGarduate.toLowerCase() === 'n') {
-  alert('Wrong! I do have a graduate degree.');
-  console.log('Wrong! I do have a graduate degree.');
-} else {
-  alert('You input is invalid');
-  console.log('Invalid input:', isGarduate);
-}
-
-var isEmployed  = prompt('Do i have a Job Now? Please enter yes/no');
-console.log('Has a Job?:', isEmployed );
-if (isEmployed.toLowerCase() === 'yes' || isEmployed.toLowerCase() === 'y') {
-  alert('Correct, i have a job');
-  console.log('Correct, i have a job');
-  noOfCorrectAns += 1;
-} else if (isEmployed.toLowerCase() === 'no' || isEmployed.toLowerCase() === 'n') {
-  alert('Wrong! I have a job');
-  console.log('Wrong! I have a job');
-} else {
-  alert('You input is invalid');
-  console.log('Invalid input:', isEmployed);
-}
-
-var isAvaliableForHire  = prompt('Am i looking for a new Job? Please enter yes/no');
-console.log('Looking for a new Job?:', isAvaliableForHire );
-if (isAvaliableForHire.toLowerCase() === 'yes' || isAvaliableForHire.toLowerCase() === 'y') {
-  alert('Wrong! i am not looking for a Job ');
-  console.log('Wrong! i am not looking for a Job');
-} else if (isAvaliableForHire.toLowerCase() === 'no' || isAvaliableForHire.toLowerCase() === 'n') {
-  alert('Correct, not looking for a job.');
-  console.log('Correct, not looking for a job');
-  noOfCorrectAns += 1;
-} else {
-  alert('You input is invalid');
-  console.log('Invalid input:', isAvaliableForHire);
-}
-
-var noOfTries = 4;
-var myActualFavNumber = 6;
-for (var i=0; i < noOfTries; i++) {
-  console.log('Fav Number Attempt', i+1);
-  var myFavNumber = parseInt(prompt('What is my favourite number? Please enter a number'));
-  console.log('Fav Number Ans', myFavNumber);
-  if(myFavNumber === myActualFavNumber){
-    alert('You got it');
-    console.log('You got my fav number');
+for(var i=0; i<rightAnswer.length; i++){
+  if (getAnswer.toLowerCase() === rightAnswer[i].toLowerCase()) {
+    rightAnswerFlag = true;
+    returnMessage = "Great!!! Right answer.";
     noOfCorrectAns += 1;
     break;
-  } else {
-    if(isNaN(myFavNumber)){
-      alert('You have not entered a number.')
-    } else if(myFavNumber < myActualFavNumber){
-      alert('Too Low');
-    } else if(myFavNumber > myActualFavNumber){
-      alert('Too High');
-    }  
-    if(i === noOfTries - 1)
-      alert('Wrong! May be next time');
-    else
-      alert('You can try again');
-  }
+  } 
 }
-
-var noOfTriesForFavPlace = 6;
-var myFavPlaces = ['india','seattle','hyderabad','bellevue', 'california'];
-var foundFavPlace = false;
-for (var i=0; i < noOfTriesForFavPlace; i++) {
-  console.log('Fav Place Attempt', i+1);
-  var myFavPlace = prompt('What is my favourite Place?');
-  console.log('Fav Place Ans', myFavPlace);
-
-  for(var j = 0; j < myFavPlaces.length; j++) {
-    if(myFavPlace.toLowerCase() === myFavPlaces[j].toLowerCase()) {
-      alert('You got it');
-      console.log('You got my fav Place');
-      noOfCorrectAns += 1;
-      foundFavPlace = true;
+if(!rightAnswerFlag) {
+  for(var j=0; j<otherValidResponses.length; j++) {
+    if (getAnswer.toLowerCase() === otherValidResponses[j].toLowerCase()) {
+      validResponseFlag = true;
+      returnMessage = "Incorrect answer.";
       break;
     } 
   }
-  if(foundFavPlace)
-    break;
-
-  if(i === noOfTriesForFavPlace - 1)
-    alert('Wrong! May be next time');
-  else
-    alert('You can try again');  
 }
+if(!rightAnswerFlag && !validResponseFlag) {
+  returnMessage = "Invalid input. Please enter Yes (YES/Y/y) or No (NO/N/n). "; 
+}
+console.log('returnMessage for Question', question, "is ", returnMessage);
+return returnMessage;
+}
+
+var displayMessage; 
+var yesAnswer = ["Y", "YES"];
+var noAnswer = ["N", "NO"];
+
+alert((promptYesOrNoQuestionAboutMe("Do you think i am over 18 years old?", yesAnswer, noAnswer)));
+alert((promptYesOrNoQuestionAboutMe("Am i a US Citizen?", noAnswer, yesAnswer)));
+alert((promptYesOrNoQuestionAboutMe("Do i have a Graduate degree?", yesAnswer, noAnswer)));
+alert((promptYesOrNoQuestionAboutMe("Do i have a Job Now?", yesAnswer, noAnswer)));
+alert((promptYesOrNoQuestionAboutMe("Am i looking for a new Job", noAnswer, yesAnswer)));
+
+function guessFavNumber(myActualFavNumber){
+  var noOfTries = 4;
+  for (var i=0; i < noOfTries; i++) {
+    console.log('Fav Number Attempt', i+1);
+    var myFavNumber = parseInt(prompt('What is my favourite number? Please enter a number'));
+    console.log('Fav Number Ans', myFavNumber);
+    if(myFavNumber === myActualFavNumber){
+      alert('You got it');
+      console.log('You got my fav number');
+      noOfCorrectAns += 1;
+      break;
+    } else {
+      if(isNaN(myFavNumber)){
+        alert('You have not entered a number.')
+      } else if(myFavNumber < myActualFavNumber){
+        alert('Too Low');
+      } else if(myFavNumber > myActualFavNumber){
+        alert('Too High');
+      }  
+      if(i === noOfTries - 1)
+        alert('Wrong! May be next time');
+      else
+        alert('You can try again');
+    }
+  }
+}
+guessFavNumber(6);
+
+
+function guessFavPlace(myFavPlaces, noOfTriesForFavPlace) {
+
+  var foundFavPlace = false;
+  for (var i=0; i < noOfTriesForFavPlace; i++) {
+    console.log('Fav Place Attempt', i+1);
+    var myFavPlace = prompt('What is my favourite Place?');
+    console.log('Fav Place Ans', myFavPlace);
+  
+    for(var j = 0; j < myFavPlaces.length; j++) {
+      if(myFavPlace.toLowerCase() === myFavPlaces[j].toLowerCase()) {
+        alert('You got it');
+        console.log('You got my fav Place');
+        noOfCorrectAns += 1;
+        foundFavPlace = true;
+        break;
+      } 
+    }
+    if(foundFavPlace)
+      break;
+  
+    if(i === noOfTriesForFavPlace - 1)
+      alert('Wrong! May be next time');
+    else
+      alert('You can try again');  
+  }
+}
+
+var myFavoritePlaces = ['india','seattle','hyderabad','bellevue', 'california'];
+guessFavPlace(myFavoritePlaces, 6);
 
 console.log('noOfCorrectAns::', noOfCorrectAns);
 alert('You got '+ noOfCorrectAns + ' out of 7 Questions. '+userName+'! Better luck next time!');
